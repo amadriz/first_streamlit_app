052cdc1b84e290f5efa0833ce17bf30e4e02aa7e
amadriz <ahovenga@gmail.com>
2023-10-02 15:18:59 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,11 +8,13 @@ import pandas
 
 my_fruit_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+ftuits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Pick up the fruit they want to include by fruit name
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries','Banana'])
 
 
 # Display the table
-streamlit.dataframe(my_fruit_list)
+# streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_show)
 
