a339538f723bc2fc085ee165ec61639edb585856
amadriz <ahovenga@gmail.com>
2023-10-02 15:22:13 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import pandas
 # streamlit.text('Hard-Boiled Free-Range Egg')
 
 my_fruit_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+# my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 # Pick up the fruit they want to include by fruit name
