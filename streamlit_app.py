1c8c10c8534aadebf2d78ee9d3bbc485cc5b3cf2
amadriz <ahovenga@gmail.com>
2023-10-02 15:25:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,15 +7,15 @@ import pandas
 # streamlit.text('Hard-Boiled Free-Range Egg')
 
 my_fruit_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-# my_fruit_list = my_fruit_list.set_index('Fruit')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
-# Pick up the fruit they want to include by fruit name
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries','Banana'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+# # Pick up the fruit they want to include by fruit name
+# fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries','Banana'])
+# fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
 # Display the table
-# streamlit.dataframe(my_fruit_list)
-streamlit.dataframe(fruit_to_show)
+streamlit.dataframe(my_fruit_list)
+# streamlit.dataframe(fruit_to_show)
 
