94743d115d7388a65742023fa4b540f5b86e791d
amadriz <ahovenga@gmail.com>
2023-10-03 21:08:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,24 +5,28 @@ import snowflake.connector
 from urllib.error import URLError
 
 
-
-
-
-
 my_fruit_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Input 
+# new code
 
+def get_fruityvice_data(this_fruit_choice):
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   return fruityvice_normalized
+
+streamlit.header('Fruityvice Fruit Advice')
 try:
   fruit_choice = streamlit.text_input('What fruit would yo like info about?', 'kiwi')
   if not fruit_choice:
     streamlit.error("Please select a fruit")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+
+
     # dataframe will put the info in a table
-    streamlit.dataframe(fruityvice_normalized)
+    # streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
   streamlit.error()
